84db7119c8faf8479fbf692050342e7d5499aa84
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-09-29 22:39:34 -0700

    Process user input, print correct selection

--- processes.c
+++ processes.c
@@ -1,9 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]) {
+typedef struct
+{
+    struct Pcb* parent;
+    struct Pcb* child;
 
-    printf("Hello");
+} Pcb;
+
+int main(void) {
+
+    char input;
+
+    do {
+        printf("\nPlease make your selection:\n");
+        printf("1) Initialize process hierarchy\n");
+        printf("2) Create a new child process\n");
+        printf("3) Destroy all descendants of parent process\n");
+        printf("4) Quit program and free memory\n\n");
+
+        printf("Input: ");
+        input = getchar();
+        while (getchar() != '\n');
+
+        switch (input)
+        {
+        case '1':
+            printf("Initializing process hierarchy...\n");
+            break;
+        case '2':
+            printf("Creating new child process...\n");
+            break;
+        case '3':
+            printf("Destroying all descendants of parent process...\n");
+            break;
+        case '4':
+            printf("Quitting program and freeing memory...\n");
+            break;
+        default:
+            printf("INVALID INPUT! Please re-enter input...\n");
+            break;
+        }
+    } while (input != '4');
+
 
     return 0;
 }
