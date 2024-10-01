c2388b18015691b1589c28666f2457847dbde84f
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-09-30 19:45:56 -0700

    Implemented print & initializing PCB

--- processes.c
+++ processes.c
@@ -1,12 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
-{
-    struct Pcb* parent;
-    struct Pcb* child;
+#define MAX_PROCESSES 10
 
-} Pcb;
+struct Child{
+    int childId;
+    struct Child* sibling;
+};//end Child
+typedef struct {
+    int parentId;
+    struct Child* child;
+
+} PCB; //end PCB
+
+PCB *allPCB[MAX_PROCESSES];
+
+void printProcessHierarchy() {
+   struct Child *ptr;
+
+    printf("\nProcess list:\n");
+    for(int i = 0; i < MAX_PROCESSES && allPCB[i] != NULL; i++) {
+        printf("Process id: %d\n", i);
+
+        if(allPCB[i]->parentId == -1)
+            printf("\tNo parent process\n");
+        else
+            printf("\tParent process: %d\n", allPCB[i]->parentId);
+
+        if(allPCB[i]->child == NULL)
+            printf("\tNo child process\n");
+        else
+            ptr = allPCB[i]->child;
+            while(ptr != NULL) {
+                printf("\tChild process: %d\n", ptr->childId);
+                ptr = ptr->sibling;
+            }
+
+    }
+}// end printProcessHierarchy
+
+void initializeProcessHierarchy() {
+    allPCB[0] = (PCB*)malloc(sizeof(PCB));
+    allPCB[0]->parentId = -1;
+    allPCB[0]->child = NULL;
+    for(int i = 1; i < MAX_PROCESSES; i++) {
+        allPCB[i] = NULL;
+    }
+    printProcessHierarchy();
+
+}// end initializeProcessHierarchy
+
 
 int main(void) {
 
@@ -14,6 +57,7 @@ int main(void) {
 
     do {
         printf("\nPlease make your selection:\n");
+        printf("=====================================\n");
         printf("1) Initialize process hierarchy\n");
         printf("2) Create a new child process\n");
         printf("3) Destroy all descendants of parent process\n");
@@ -27,6 +71,7 @@ int main(void) {
         {
         case '1':
             printf("Initializing process hierarchy...\n");
+            initializeProcessHierarchy();
             break;
         case '2':
             printf("Creating new child process...\n");
@@ -43,7 +88,6 @@ int main(void) {
         }
     } while (input != '4');
 
-
-    return 0;
-}
+    return 1;
+}//end main
 
