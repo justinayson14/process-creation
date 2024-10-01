63de9748137b36c23f44030940d089c96a41d03e
Justin Ayson <121000453+justinayson14@users.noreply.github.com>
2024-09-30 23:52:50 -0700

    Defined destroy functions; Fixed print function not printing non null PCB[]

--- processes.c
+++ processes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_PROCESSES 5
+#define MAX_PROCESSES 10
 
 struct Child{
     int childId;
@@ -19,7 +19,10 @@ void printProcessHierarchy() {
    struct Child *ptr;
 
     printf("\nProcess list:\n");
-    for(int i = 0; i < MAX_PROCESSES && allPCB[i] != NULL; i++) {
+    for(int i = 0; i < MAX_PROCESSES; i++) {
+        if (allPCB[i] == NULL)
+            continue;
+            
         printf("Process id: %d\n", i);
 
         if(allPCB[i]->parentId == -1)
@@ -31,7 +34,7 @@ void printProcessHierarchy() {
             printf("\tNo child process\n");
         else {
             ptr = allPCB[i]->child;
-            while(ptr != NULL) {
+            while(ptr != NULL) { // print all child of i (each child linked to its sibling)
                 printf("\tChild process: %d\n", ptr->childId);
                 ptr = ptr->sibling;
             }// end while
@@ -42,14 +45,17 @@ void printProcessHierarchy() {
 }// end printProcessHierarchy
 
 void initializeProcessHierarchy() {
+    // initialize first parent PCB
     allPCB[0] = (PCB*)malloc(sizeof(PCB));
     allPCB[0]->parentId = -1;
     allPCB[0]->child = NULL;
+
+    // initialize other PCB as null
     for(int i = 1; i < MAX_PROCESSES; i++) {
         allPCB[i] = NULL;
-    }
+    } // end for
+    
     printProcessHierarchy();
-
     return;
 }// end initializeProcessHierarchy
 
@@ -61,40 +67,84 @@ void createChild() {
     while (getchar() != '\n');
 
     if (allPCB[p] == NULL) { 
-        printf("\nERROR: Process does not exist. Try again.\n");
+        printf("\nERROR: Parent process does not exist. Try again.\n");
         return;
-    }
+    }//end if
 
+    // q starts 1 after parent (all PCBs stored in array)
     for(q = p+1; q <= MAX_PROCESSES; q++) {
         if(allPCB[q] == NULL)
             break;
-    }
+    }// end for
     if (q == MAX_PROCESSES) {
         printf("\nERROR: No PCB's are available. Try again.\n");
         return;
-    }
+    }// end for
 
+    // initialize PCB[q]
     allPCB[q] = (PCB*)malloc(sizeof(PCB));
     allPCB[q]->parentId = p;
     allPCB[q]->child = NULL; 
 
+    // initialize Child q
     struct Child *temp = (struct Child*)malloc(sizeof(struct Child));
     temp->childId = q;
     temp->sibling = NULL;
 
+    // p no child, make q first child
     if (allPCB[p]->child == NULL)
         allPCB[p]->child = temp;
-    else {
+    else { // p has child, make q sibling of p's youngest child
         struct Child *ptr = allPCB[p]->child;
-        while(ptr->sibling != NULL)
+        while(ptr->sibling != NULL) // find youngest child
             ptr = ptr->sibling;
         
         ptr->sibling = temp;
-    }
+    }// end else
 
     printProcessHierarchy();
     return;
 
+}// end createChild
+
+void destroyChildren(struct Child *ptr) {
+
+    if (ptr == NULL) // if end of linkedlist, return
+        return;
+    else {
+        destroyChildren(ptr->sibling); // recursive to next node
+        int q = ptr->childId;
+        destroyChildren(allPCB[q]->child); // recursive to child of current node
+        free(allPCB[q]);
+        allPCB[q] = NULL;
+        free(ptr); // free Child struct of ptr
+        ptr = NULL;
+    }// end else
+    
+}// end destroyChildren
+
+void destroyDescendents() {
+    int p;
+
+    printf("Enter parent process whose descendents are to be destroyed: ");
+    scanf("%d", &p);
+    while (getchar() != '\n');
+
+    destroyChildren(allPCB[p]->child);
+    allPCB[p]->child = NULL; // reset children of PCB[p] to null
+    
+    printProcessHierarchy();
+    return;
+}// end destroyDescendents
+
+void destroyAllPCB() {
+    if (allPCB[0] != NULL) {
+        if (allPCB[0]->child != NULL)
+            destroyChildren(allPCB[0]->child);
+        for(int i = 0; i < MAX_PROCESSES && allPCB[i] != NULL; i++)
+            free(allPCB[i]);
+    }
+    return;
 }
 
 int main(void) {
@@ -111,29 +161,31 @@ int main(void) {
 
         printf("Selection: ");
         input = getchar();
-        while (getchar() != '\n');
+        while (getchar() != '\n'); // clear input buffer
 
         switch (input)
         {
         case '1':
-            printf("\nInitializing process hierarchy...\n");
+            printf("Initializing process hierarchy...\n");
             initializeProcessHierarchy();
             break;
         case '2':
-            printf("\nCreating new child process...\n");
+            printf("Creating new child process...\n");
             createChild();
             break;
         case '3':
-            printf("\nDestroying all descendants of parent process...\n");
+            printf("Destroying all descendants of parent process...\n");
+            destroyDescendents();
             break;
         case '4':
-            printf("\nQuitting program and freeing memory...\n");
+            printf("Quitting program and freeing memory...\n");
+            destroyAllPCB();
             break;
         default:
-            printf("\nINVALID INPUT! Please re-enter input...\n");
+            printf("INVALID INPUT! Please re-enter input...\n");
             break;
-        }
-    } while (input != '4');
+        }// end switch
+    } while (input != '4'); // end do while
 
     return 1;
 }//end main
